9ceb485fef087e3d0d944d377423164dd78130fd
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2018-02-17 19:41:42 +0200

    Allow default values and nullable values

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,8 +1,8 @@
-import { requireConfigVars } from './index';
+import { requireConfigKeys, requireConfigVars, Config } from './index';
 
 let originalEnv: NodeJS.ProcessEnv;
 
-describe('baseRequire', () => {
+describe('requireConfigKeys', () => {
   beforeAll(() => {
     // store the original env
     originalEnv = { ...process.env };
@@ -15,10 +15,117 @@ describe('baseRequire', () => {
 
   it('should require a variable that has no default or is not nullable', () => {
     process.env = {};
-    const missingEnvVars = requireConfigVars({ FOO: process.env.FOO });
+    const config: Config = { UNDEFINED: process.env.UNDEFINED };
+    const missingEnvVars = requireConfigKeys(config);
     expect(missingEnvVars).toHaveLength(1);
-    expect(missingEnvVars).toContainEqual('FOO');
+    expect(missingEnvVars).toContain('UNDEFINED');
   });
-  it('should not require a variable that has a default specified');
-  it('should not require a variable that is nullable');
+
+  it('should not require a variable that has a default specified', () => {
+    process.env = {};
+    const config: Config = {
+      HAS_DEFAULT_VALUE: process.env.HAS_DEFAULT_VALUE || 'DEFAULT',
+    };
+    const missingEnvVars = requireConfigKeys(config);
+    expect(missingEnvVars).toHaveLength(0);
+    expect(missingEnvVars).not.toContain('HAS_DEFAULT_VALUE');
+  });
+
+  it('should not require a variable that is nullable', () => {
+    process.env = {};
+    const config: Config = {
+      NULLABLE: process.env.NULLABLE || null,
+    };
+    const missingEnvVars = requireConfigKeys(config);
+    expect(missingEnvVars).toHaveLength(0);
+    expect(missingEnvVars).not.toContain('NULLABLE');
+  });
+
+  it('should return multiple undefined env vars', () => {
+    process.env = {};
+    const config: Config = {
+      REQUIRED_1: process.env.REQUIRED_1,
+      REQUIRED_2: process.env.REQUIRED_2,
+    };
+    const missingEnvVars = requireConfigKeys(config);
+    expect(missingEnvVars).toHaveLength(2);
+    expect(missingEnvVars).toContain('REQUIRED_1');
+    expect(missingEnvVars).toContain('REQUIRED_2');
+  });
+  it('should not return anything if all are defined', () => {
+    process.env = {
+      REQUIRED_1: 'foo',
+      REQUIRED_2: 'bar',
+    };
+    const config: Config = {
+      REQUIRED_1: process.env.REQUIRED_1,
+      REQUIRED_2: process.env.REQUIRED_2,
+      FOO: 'baz',
+    };
+    const missingEnvVars = requireConfigKeys(config);
+    expect(missingEnvVars).toHaveLength(0);
+  });
+  // it('should allow objects');
+});
+
+describe('requireConfigVars', () => {
+  beforeAll(() => {
+    // store the original env
+    originalEnv = { ...process.env };
+  });
+
+  afterEach(() => {
+    // reset to original env
+    process.env = originalEnv;
+  });
+
+  it('should return a string array', () => {
+    process.env = {};
+    const config: Config = {
+      REQUIRED_1: process.env.REQUIRED_1,
+      REQUIRED_2: process.env.REQUIRED_2,
+      FOO: process.env.FOO || 'foo',
+      BAR: process.env.BAR || null,
+    };
+    const missingEnvVars = requireConfigVars(config);
+    expect(missingEnvVars).toHaveLength(2);
+    expect(missingEnvVars).toContain('REQUIRED_1');
+    expect(missingEnvVars).toContain('REQUIRED_2');
+  });
+
+  it('should flatten the results from all configs to string array', () => {
+    process.env = {};
+    const config: Config = {
+      REQUIRED_1: process.env.REQUIRED_1,
+      REQUIRED_2: process.env.REQUIRED_2,
+      FOO: process.env.FOO || 'foo',
+      BAR: process.env.BAR || null,
+    };
+    const config2: Config = {
+      FOO: process.env.FOO,
+      BAR: process.env.BAR,
+    };
+    const missingEnvVars = requireConfigVars(config, config2);
+    expect(missingEnvVars).toHaveLength(4);
+    expect(missingEnvVars).toContain('REQUIRED_1');
+    expect(missingEnvVars).toContain('REQUIRED_2');
+    expect(missingEnvVars).toContain('FOO');
+    expect(missingEnvVars).toContain('BAR');
+  });
+
+  it('should remove duplicates', () => {
+    process.env = {};
+    const config: Config = {
+      FOO: process.env.FOO,
+      BAR: process.env.BAR,
+    };
+    const config2: Config = {
+      FOO: process.env.FOO,
+      BAR: process.env.BAR,
+    };
+    const missingEnvVars = requireConfigVars(config, config2);
+    expect(missingEnvVars).toHaveLength(2);
+    expect(missingEnvVars).toContain('FOO');
+    expect(missingEnvVars).toContain('BAR');
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,26 @@ export interface Config {
   [key: string]: string | null;
 }
 
-export const requireConfigVars = (config: Config) => {
+export const requireConfigKeys = (config: Config) => {
   const missingEnvVars: string[] = [];
   Object.keys(config).forEach(key => {
-    if (!process.env[key]) missingEnvVars.push(key);
+    // Allow null values
+    if (process.env[key] === undefined && config[key] !== null) {
+      // Allow config keys with a default value
+      if (typeof config[key] !== 'string') {
+        missingEnvVars.push(key);
+      }
+    }
   });
   return missingEnvVars;
 };
 
-export const baseRequire = (...config: Config[]) => {};
+const unique = <T>(arr: T[]) => Array.from(new Set(arr));
+
+export const requireConfigVars = (...configs: Config[]): string[] => {
+  return unique(
+    configs.map(requireConfigKeys).reduce((acc, arr) => acc.concat(arr)),
+  );
+};
 
 export default requireConfigVars;
